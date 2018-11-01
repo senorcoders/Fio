57e02ace2f9ad0ad3cabd4ecf7765ca540f72c64
Dayana Lara <dlara@SenorCoders-2.localdomain>
2018-11-01 18:56:58 +0000

    SQL Database added

--- wp-content/themes/fioblockchain/footer.php
+++ wp-content/themes/fioblockchain/footer.php
@@ -76,6 +76,9 @@
   <li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
   <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
   <li class="list-inline-item"><a href="#"><i class="fa fa-medium" aria-hidden="true"></i></a></li>
+  <li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li class="list-inline-item"><a href="#"><i class="fa fa-reddit" aria-hidden="true"></i></a></li>
+      <li class="list-inline-item"><a href="#"><i class="fa fa-telegram" aria-hidden="true"></i></a></li>
 </ul>
        </div>
     </div>
--- wp-content/themes/fioblockchain/inc/top-header.php
+++ wp-content/themes/fioblockchain/inc/top-header.php
@@ -1,11 +1,15 @@
 <div class="row">
-  <div class="col-lg-7"></div>
-  <div class="col-lg-5">
+  <div class="col-lg-6"></div>
+  <div class="col-lg-6">
     <ul class="social-header list-inline">
   <li class="list-inline-item underline"><a href="#">Contact Us</a></li>
   <li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
   <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
   <li class="list-inline-item"><a href="#"><i class="fa fa-medium" aria-hidden="true"></i></a></li>
+  <li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li class="list-inline-item"><a href="#"><i class="fa fa-reddit" aria-hidden="true"></i></a></li>
+      <li class="list-inline-item"><a href="#"><i class="fa fa-telegram" aria-hidden="true"></i></a></li>
+
 </ul>
   </div>
 </div>
